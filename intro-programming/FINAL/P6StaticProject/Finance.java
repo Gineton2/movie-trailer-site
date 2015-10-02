206fbb32ea45951f29315dfdb1eb3e3f7e079b8a
Gineton Alencar II <gineton2@gmail.com>
2015-10-01 17:55:20 -0700

    Cleaned up udacity repo clone for project

--- intro-programming/FINAL/P6StaticProject/Finance.java
+++
@@ -1,9 +0,0 @@
-
-public class Finance
-{
-    public static double getTip(double costOfDinner)
-    {
-        final double TIP_RATE = .2;
-        return costOfDinner * TIP_RATE;
-    }
-}
--- intro-programming/FINAL/Q3ExtendingRectangles/Ud.java
+++
@@ -1,5 +0,0 @@
-// The Rectangle in the graphics package we have been using this semester is limited
-// because it can not calculate its area.
-// Create a class UdacityRectagle that extends Rectagle and adds an area() method
-// You will not need to provide any new instance variables, but you will need a constructor
-//
